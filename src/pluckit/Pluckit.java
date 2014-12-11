fda0ebe7a33aba91a2c8da5920dd43786ecdc135
Doug Hogg <dougis.hogg@live.com>
2014-12-11 13:37:38 -0700

    This is the end again

--- src/pluckit/Pluckit.java
+++ src/pluckit/Pluckit.java
@@ -5,6 +5,11 @@
  */
 package pluckit;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,6 +22,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -25,6 +31,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
 
 /**
  *
@@ -50,7 +57,7 @@ public class Pluckit extends Application {
         Menu file = new Menu("File");
         Menu edit = new Menu("Edit");
         Menu tools = new Menu("tools");
-        
+     
         menubar.getMenus().addAll(file,edit,tools);
       
         
@@ -178,27 +185,26 @@ public class Pluckit extends Application {
         HBox gg = new HBox();
         gg.getChildren().addAll(g1,g2,g3);
         
-        
+       
         
         VBox vbox = new VBox();
         VBox vbox2 = new VBox();
         VBox main3 = new VBox();
         vbox.getChildren().addAll(a,b,c,d,e,f,g);
         vbox2.getChildren().addAll(aa,bb,cc,dd,ee,ff,gg);
-        main3.getChildren().addAll(vbox,vbox2);
+        main3.getChildren().addAll(vbox);
         Insets i = new Insets(0,0,50,0);
         vbox.setPadding(i);
         
       
    
-        FlowPane flow = new FlowPane();
  
         a1.setOnAction(new EventHandler<ActionEvent>() {
             double pos = 0;
             @Override
             public void handle(ActionEvent t) {
-                 flow.getChildren().add(new ImageView(new Image("/makeplace.png"))); 
-                 holderArea();
+               
+               
             }
         });
       
@@ -309,18 +315,25 @@ public class Pluckit extends Application {
         */
         HBox a = new HBox();
         a.getChildren().addAll(a4,a5,a6);
+        a.setSpacing(10);
         HBox b = new HBox();
         b.getChildren().addAll(b4,b5,b6);
+        b.setSpacing(10);
         HBox c = new HBox();
         c.getChildren().addAll(c4,c5,c6);
+        c.setSpacing(10);
         HBox d = new HBox();
         d.getChildren().addAll(d4,d5,d6);
+        d.setSpacing(10);
         HBox e = new HBox();
         e.getChildren().addAll(e4,e5,e6);
+        e.setSpacing(10);
         HBox f = new HBox();
         f.getChildren().addAll(f4,f5,f6);
+        f.setSpacing(10);
         HBox g = new HBox();
         g.getChildren().addAll(g4,g5,g6);
+        g.setSpacing(10);
          HBox aa = new HBox();
         aa.getChildren().addAll(a1,a2,a3);
         HBox bb = new HBox();
@@ -336,43 +349,71 @@ public class Pluckit extends Application {
         HBox gg = new HBox();
         gg.getChildren().addAll(g1,g2,g3);
         
+        HBox pics = new HBox(10);
+        pics.getChildren().addAll(new ImageView(new Image("/solidCircle.png")),
+                new ImageView(new Image("/hollowCircle.png")),
+                new ImageView(new Image("/solidTriangle.png")),
+                new ImageView(new Image("/hollowSquare.png")));
         
-        
+      
         VBox vbox = new VBox();
         VBox vbox2 = new VBox();
         VBox main3 = new VBox();
-        vbox.getChildren().addAll(a,b,c,d,e,f,g);
-        vbox2.getChildren().addAll(aa,bb,cc,dd,ee,ff,gg);
-        main3.getChildren().addAll(vbox,vbox2);
-        Insets i = new Insets(0,0,50,0);
-        vbox.setPadding(i);
         
-      
-   
-        FlowPane flow = new FlowPane();
- 
-        a1.setOnAction(new EventHandler<ActionEvent>() {
-            double pos = 0;
+        vbox.getChildren().addAll(a,b,c,d,e,f,g);
+        vbox.setSpacing(10);
+        vbox2.getChildren().addAll(aa,bb,cc,dd,ee,ff,gg);
+        main3.getChildren().addAll(vbox,pics);
+         Insets i = new Insets(0,0,50,0);
+        vbox.setPadding(i);
+
+        int[] chord = new int[6];
+        chord[0] = 0;
+        chord[1] = 2;
+        chord[2] = 1;
+        chord[3] = 2;
+        chord[4] = 0;
+        chord[5] = 2;
+        
+         int[] chord2 = new int[6];
+        chord2[0] = 0;
+        chord2[1] = 6;
+        chord2[2] = 4;
+        chord2[3] = 3;
+        chord2[4] = 2;
+        chord2[5] = 0;
+        
+        final TestTab tab = new TestTab();
+        a4.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
-                 flow.getChildren().addAll(new StackPane(new Rectangle(200, 400, Color.CORNFLOWERBLUE),new ImageView(new Image("/makeplace.png")))); 
-                 holderArea();
+                
+                tab.getChild(chord);
             }
         });
-           /****************************************************/
-        
+         a5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                
+                tab.getChild(chord2);
+            }
+        });
+          a6.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                
+                tab.getChild(chord);
+            }
+        });
+          
          
-           flow.setVgap(20);
-           flow.setHgap(20);
-
-        
         /****************************************************/
            ScrollPane holderArea = new ScrollPane();
            holderArea.setFitToWidth(true);
            ScrollPane sections = new ScrollPane();
            sections.setPrefWidth(225);
            sections.setContent(main3);
-           holderArea.setContent(flow);
+           holderArea.setContent(tab.getTab());
       
            BorderPane wholeArea = new BorderPane();
   
